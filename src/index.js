48d8f31ad9c351f917d67ce6ce91b1e9719713d8
Thomas <tcarbonell12@gmail.com>
2025-05-24 00:25:51 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form")
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();//prevents page reload after submission
+
+    //selects the inputted text from the text field
+    const taskDescription = document.getElementById("new-task-description").value;
+  
+    buildToDo(taskDescription);
+  })
 });
+
+
+function buildToDo(task){
+
+  const taskList = document.getElementById("tasks");
+  const li = document.createElement("li");
+  
+
+  li.textContent = task;
+  taskList.appendChild(li);
+
+  
+  console.log(task);
+}
